2c3bb1093f6093cb5e0c3b4250c5bd7b77e0aa5d
paulAjii1234 <paul@grangerpr.com>
2024-08-08 23:36:32 +0100

    Update

--- Mistletoe Studio/script.js
+++ Mistletoe Studio/script.js
@@ -3,6 +3,12 @@ const animatableBox = document.querySelectorAll(".animatable")
 const sessionModalBtns = document.querySelectorAll(".session__modal-btn")
 const sessionModal = document.querySelector(".session__modal")
 
+// Gallery Modal
+const galleryModal = document.querySelector(".gallery__modal")
+const leftArrow = document.querySelector(".gallery__modal .bxs-left-arrow")
+const rightArrow = document.querySelector(".gallery__modal .bxs-right-arrow")
+const galleryImgs = document.querySelectorAll(".gallery__wrapper img")
+
 // Footer Date
 document.querySelector(".date").innerHTML = new Date().getFullYear()
 
@@ -43,5 +49,43 @@ sessionModalBtns.forEach(btn => {
 window.addEventListener("click", (e) => {
     if (e.target === sessionModal) {
         closeModal(sessionModal)
+    }
+
+    if (e.target === galleryModal) {
+        closeModal(galleryModal)
+    }
+})
+
+// Gallery Modal Functioning
+let currentIndex = 0
+const totalImgs = galleryImgs.length
+
+const showGalleryModal = (i) => {
+    const modalImgs = document.querySelectorAll(".gallery__img")
+    modalImgs.forEach(img => {
+        img.style.opacity = "0"
+    })
+    modalImgs[i].style.opacity = "1"
+}
+
+galleryImgs.forEach((galleryImg, index) => {
+    galleryImg.addEventListener("click", (e) => {
+        currentIndex = index
+        openModal(galleryModal)
+        showGalleryModal(currentIndex)
+    })
+})
+
+rightArrow.addEventListener("click", () => {
+    if (currentIndex < (totalImgs - 1)) {
+        currentIndex++
+        showGalleryModal(currentIndex)
+    }
+})
+
+leftArrow.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--
+        showGalleryModal(currentIndex)
     }
 })
